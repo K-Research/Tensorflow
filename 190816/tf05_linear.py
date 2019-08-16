29b12cbe427fd4ba816ca5fc982b4f822e7ce06e
Taewhan Kim <lechevalier@naver.com>
2019-08-16 12:42:58 +0900

    Add files via upload

--- 190816/tf05_linear.py
+++ 190816/tf05_linear.py
@@ -12,18 +12,18 @@ b = tf.Variable(tf.random_normal([1]), name = 'bias')
 hypothesis = x_train * W + b
 
 # cost / loss function
-cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+cost = tf.reduce_mean(tf.square(hypothesis - y_train)) # loss = 'mse'
 
 # optimizer
-train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
+train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost) # optimizer = 'GD'
 
 # Launch the graph in a session.
 with tf.Session() as sess:
     # Initializes global variables in the graph.
-    sess.run(tf.global_variables_initializer())
+    sess.run(tf.global_variables_initializer()) # tensorflow는 모델의 다른 연산을 실행하기 전에 반드시 명시적으로 초기화를 해야한다.
 
     # Fit the line
-    for step in range(2001):
+    for step in range(2001): # epoch = 2000
         _, cost_val, W_val, b_val = sess.run([train, cost, W, b])
 
         if step % 20 == 0:
