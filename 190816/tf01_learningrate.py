4417c35075726c82097460139772ed2a084e4dc5
Taewhan Kim <lechevalier@naver.com>
2019-08-16 10:57:55 +0900

    Add files via upload

--- 190816/tf01_learningrate.py
+++ 190816/tf01_learningrate.py
@@ -15,9 +15,16 @@ model.add(Dense(4))
 model.add(Dense(1))
 
 # 3. 훈련
-from keras.optimizers import Adam
+from keras.optimizers import Adadelta, Adagrad, Adam, Adamax, Nadam, RMSprop, SGD
 
+# optimizer = Adadelta(lr = 1.8)
+# optimizer = Adagrad(lr = 0.1)
 optimizer = Adam(lr = 0.01)
+# optimizer = Adamax(lr = 0.1)
+# optimizer = Nadam(lr = 0.01)
+# optimizer = RMSprop(lr = 0.008269535)
+# optimizer = SGD(lr = 0.01)
+
 # model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse'])
 model.compile(loss = 'mse', optimizer = optimizer, metrics = ['mse'])
 model.fit(x, y, epochs = 100, batch_size = 1)
