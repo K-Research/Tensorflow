f84c582d5cd2ebdc99c757f12163ae68ec522d99
Taehwan Kim <lechevalier@naver.com>
2019-08-19 09:40:55 +0900

    Add files via upload

---
+++ 190819/tf06_variable.py
@@ -0,0 +1,16 @@
+# 랜덤값으로 변수 1개를 만들고, 변수의 내용을 출력하시오.
+
+import tensorflow as tf
+
+# tf.set_random_seed(777)
+
+W = tf.Variable(tf.random_normal([1]), name = 'weight')
+b = tf.Variable(tf.random_normal([1]), name = 'bias')
+
+print(W)
+
+# W = tf.Variable([0.3], tf.float32)
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+print(session.run(W))
