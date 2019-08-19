22252d3ebde6b63026ec9396f7e7b5ef8c19eb03
Taehwan Kim <lechevalier@naver.com>
2019-08-19 17:05:03 +0900

    Add files via upload

--- 190819/tf09_3_breast_cancer.py
+++ 190819/tf09_3_breast_cancer.py
@@ -1,23 +1,28 @@
 # Logistic Regression Classifier
 import tensorflow as tf
+import numpy as np
 
 tf.set_random_seed(777) # for reproducibility
 
-x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
-y_data = [[0], [0], [0], [1], [1], [1]]
+xy = np.load('cancer_data.npy')
+x_data = xy[ : , 0 : -1]
+y_data = xy[ : , [-1]]
+
+print(x_data.shape, y_data.shape)
 
 # placeholders for a tensor that will be always fed.
-X = tf.placeholder(tf.float32, shape = [None, 2])
+X = tf.placeholder(tf.float32, shape = [None, 30])
 Y = tf.placeholder(tf.float32, shape = [None, 1])
 
-W = tf.Variable(tf.random_normal([2, 1]), name = 'weight')
+# W = tf.Variable(tf.random_normal([8, 1]), name = 'weight')
+W = tf.Variable(tf.random_normal([30, 1]), name = 'weight')
 b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
-# Hypothesis using sigmoid : tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
-hypothesis = tf.sigmoid(tf.matmul(X, W) + b) # activation = 'sigmoid'
+# Hypothesis using sigmoid : tf.div(1., 1. + tf.exp(-tf.matmul(X, W)))
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
 
-# cost / loss fuction 로지스틱 리그레션에서 cost에 -가 붙는다.
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis)) # loss = 'binary_crossentropy'
+# cost / loss fuction
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 
 train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
 
@@ -28,15 +33,15 @@ accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype = tf.float32))
 
 # Launch graph
 with tf.Session() as sess:
-    # Initialize TensorFlow variables
+    # Initialize Tensorflow variables
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
         cost_val, _ = sess.run([cost, train], feed_dict = {X : x_data, Y : y_data})
-        
+
         if step % 200 == 0:
             print(step, cost_val)
-
+        
     # Accuracy report
     h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict = {X : x_data, Y : y_data})
-    print("\nHypothesis : ", h, "\nCorrect (Y) : ", c, "\nAccuracy : ", a)
+    print("\nHypothesis : ", h, "\Correct (Y) : ", c, "\nAccuracy : ", a)
