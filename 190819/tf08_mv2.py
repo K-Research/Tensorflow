3ac8d0001f23e25b09c96cc7e38ea12ae3a89f15
Taehwan Kim <lechevalier@naver.com>
2019-08-19 15:12:39 +0900

    Add files via upload

--- 190819/tf08_mv2.py
+++ 190819/tf08_mv2.py
@@ -10,7 +10,7 @@ y_data = [[152.], [185.], [180.], [196.], [142.]]
 X = tf.placeholder(tf.float32, shape = [None, 3])
 Y = tf.placeholder(tf.float32, shape = [None, 1])
 
-W = tf.Variable(tf.random_normal([3, 1]), name = 'weight')
+W = tf.Variable(tf.random_normal([3, 1]), name = 'weight') # input : 3, output : 1
 b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
 # Hypothesis
