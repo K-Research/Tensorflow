f4710251586faf18a6c1cd5220064e0fd479bfd5
Taehwan Kim <lechevalier@naver.com>
2019-08-23 12:44:38 +0900

    Add files via upload

--- 190823/tf19_cifar10.py
+++ 190823/tf19_cifar10.py
@@ -1,61 +1,79 @@
-import tensorflow as tf
+from keras.datasets import cifar10
+from keras.utils import np_utils
+import numpy
 import random
-# import matplotlib.pyplot as plt
-from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+from tensorflow.keras.datasets.cifar10 import load_data
+
+# 다음 배치를 읽어오기 위한 next_batch 유틸리티 함수를 정의합니다.
+def next_batch(number, data, labels):
+  '''
+  `num` 개수 만큼의 랜덤한 샘플들과 레이블들을 리턴합니다.
+  '''
+  index = numpy.arange(0 , len(data))
+  numpy.random.shuffle(index)
+  index = index[ : number]
+  data_shuffle = [data[i] for i in index]
+  labels_shuffle = [labels[i] for i in index]
+  
+  return numpy.asarray(data_shuffle), numpy.asarray(labels_shuffle)
 
 tf.set_random_seed(777) # reproducibility
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = load_data()
 
 # hyper parameters
 learning_rate = 0.001
 training_epochs = 15
 batch_size = 100
 
+NB_CLASSES = 10
+cifar10_train_num_examples = 50000
+
+# print(x_train.shape) # (50000, 32, 32, 3)
+# print(y_train.shape) # (50000, 1)
+# print(x_test.shape) # (10000, 32, 32, 3)
+# print(y_test.shape) # (10000, 1)
+
+# scalar 형태의 레이블(0~9)을 One-hot Encoding 형태로 변환합니다.
+# y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+# y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+
+y_train_one_hot = tf.squeeze(tf.one_hot(y_train, 10), axis = 1)
+y_test_one_hot = tf.squeeze(tf.one_hot(y_test, 10), axis = 1)
+
 # input place holders
-X = tf.placeholder(tf.float32, [None, 784])
-X_img = tf.reshape(X, [-1, 28, 28, 1]) # img 28 x 28 x 1 (black / white)
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+# X_img = tf.reshape(X, [-1, 28, 28, 1]) # img 28 x 28 x 1 (black / white)
 Y = tf.placeholder(tf.float32, [None, 10])
 
-# L1 ImgIn shape = (?, 28, 28, 1)
-W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev = 0.01)) # kernel_size = (3, 3), channel = 1, output = 32
-print('W1 : ', W1)
-# Conv -> (?, 28, 28, 32)
-# Pool -> (?, 14, 14, 32)
-L1 = tf.nn.conv2d(X_img, W1, strides = [1, 1, 1, 1], padding = 'SAME')
-print('L1 : ', L1)
+# L1 ImgIn shape = (?, 32, 32, 3)
+W1 = tf.Variable(tf.random_normal([3, 3, 3, 32], stddev = 0.01))
+L1 = tf.nn.conv2d(X, W1, strides = [1, 1, 1, 1], padding = 'SAME')
 L1 = tf.nn.relu(L1)
 L1 = tf.nn.max_pool(L1, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
-print('L1 : ', L1)
 
-# L2 ImgIn shape = (?, 14, 14, 32)
+# L2 ImgIn shape = (?, 16, 16, 32)
 W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev = 0.01))
-# Conv -> (?, 14, 14, 64)
-# Pool -> (?, 7, 7, 64)
 L2 = tf.nn.conv2d(L1, W2, strides = [1, 1, 1, 1], padding = 'SAME')
 L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 
-# from tensorflow.keras import layers
-
+# L3 ImgIn shape = (?, 8, 8, 64)
 L3 = tf.layers.conv2d(L2, 64, [3, 3], activation = tf.nn.relu)
 L3 = tf.layers.max_pooling2d(L3, [2, 2], [2, 2])
 L3 = tf.layers.dropout(L3, 0.7)
 
+# L4 ImgIn shape = (?, 4, 4, 128)
 L4 = tf.contrib.layers.flatten(L3)
 L4 = tf.layers.dense(L4, 256, activation = tf.nn.relu)
 L4 = tf.layers.dropout(L4, 0.5)
 
-# L2_flat = tf.reshape(L2, [-1, 7 * 7 * 64])
 logits = tf.layers.dense(L4, 10, activation = None)
 
-# Final FC 7 x 7 x 64 inputs -> 10 outputs
-# W3 = tf.get_variable("W3", shape = [7 * 7 * 64, 10], initializer = tf.contrib.layers.xavier_initializer())
-# b = tf.Variable(tf.random_normal([10]))
-# logits = tf.matmul(L2_flat, W3) + b
-
 # define cost / loss & optimizer
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = Y))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = Y))
 optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
 
@@ -67,13 +85,14 @@ sess.run(tf.global_variables_initializer())
 print('Learning started. It takes sometime.')
 for epoch in range(training_epochs):
     avg_cost = 0
-    total_batch = int(mnist.train.num_examples / batch_size)
+    total_batch = int(cifar10_train_num_examples / batch_size)
 
     for i in range(total_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        feed_dict = {X : batch_xs, Y : batch_ys}
-        c, _ = sess.run([cost, optimizer], feed_dict = feed_dict)
-        avg_cost += c / total_batch
+            # batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            batch_xs, batch_ys = next_batch(batch_size, x_train, y_train_one_hot.eval(session = sess))
+            feed_dict = {X : batch_xs, Y : batch_ys}
+            c, _ = sess.run([cost, optimizer], feed_dict = feed_dict)
+            avg_cost += c / total_batch
 
     print('Epoch : ', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
 
@@ -82,9 +101,9 @@ print('Learning Finished')
 # Test model and check accuracy
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print('Accuracy : ', sess.run(accuracy, feed_dict = {X : mnist.test.images, Y : mnist.test.labels}))
+print('Accuracy : ', sess.run(accuracy, feed_dict = {X : x_test, Y : y_test}))
 
 # Get one and predict
-r = random.randint(0, mnist.test.num_examples - 1)
-print("Label : ", sess.run(tf.argmax(mnist.test.labels[r : r + 1], 1)))
-print("Prediction : ", sess.run(tf.argmax(logits, 1), feed_dict = {X : mnist.test.images[r : r + 1]}))
+r = random.randint(0, 10000 - 1)
+print("Label : ", sess.run(tf.argmax(x_test[r : r + 1], 1)))
+print("Prediction : ", sess.run(tf.argmax(logits, 1), feed_dict = {X : x_test[r : r + 1]}))
